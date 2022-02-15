a07bfe9f21db4ab9f248cdbf460c7c7b80ad39aa
Calvin <calvindo95@gmail.com>
2022-02-14 20:06:37 -0600

    JTN-197 feature: updated main.cpp to reflect changes in config class

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 #include <iomanip>
 #include <cstdlib>
 
+#include "Config.h"
+
 class hello_world_resource : public httpserver::http_resource {
 public:
     const std::shared_ptr<httpserver::http_response> render(const httpserver::http_request&) {
@@ -11,23 +13,15 @@ public:
 };
 
 int main(int argc, char** argv) {
-    // get environment veriables
-    int port = getenv("PORT") ? static_cast<int>(std::stoi(getenv("PORT"))) : 8080;
-    std::string https_mem_key = getenv("HTTPS_MEM_KEY_PATH") ? static_cast<std::string>(getenv("HTTPS_MEM_KEY_PATH")) : 
-        "../certs/server_ca/private/smoothstack_server.key";
-    std::string https_mem_cert = getenv("HTTPS_MEM_CERT_PATH") ? static_cast<std::string>(getenv("HTTPS_MEM_CERT_PATH")) : 
-        "../certs/server_ca/certs/smoothstack_server.crt";
-    int max_connections = getenv("MAX_CONNECTIONS") ? static_cast<int>(std::stoi(getenv("MAX_CONNECTIONS"))) : 4;
-    int connection_timeout = getenv("TIMEOUT") ? static_cast<int>(std::stoi(getenv("TIMEOUT"))) : 180;
-    int memory_limit = getenv("MEMORY_LIMIT") ? static_cast<int>(std::stoi(getenv("MEMORY_LIMIT"))) : 32768;
+    Config& config = Config::get_instance();  
 
-    httpserver::webserver ws = httpserver::create_webserver(port)
+    httpserver::webserver ws = httpserver::create_webserver(config.PORT)
         .use_ssl()
-        .https_mem_key(https_mem_key)
-        .https_mem_cert(https_mem_cert)
-        .max_connections(max_connections)
-        .connection_timeout(connection_timeout)
-        .memory_limit(memory_limit)
+        .https_mem_key(config.HTTPS_MEM_KEY)
+        .https_mem_cert(config.HTTPS_MEM_CERT)
+        .max_connections(config.MAX_CONNECTIONS)
+        .connection_timeout(config.CONNECTION_TIMEOUT)
+        .memory_limit(config.MEMORY_LIMIT)
         .use_dual_stack()
         .start_method(httpserver::http::http_utils::INTERNAL_SELECT);
 
