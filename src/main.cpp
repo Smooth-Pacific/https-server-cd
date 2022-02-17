0927276fa35e142afe29d44d9526cb2c13bf7a63
Calvin <calvindo95@gmail.com>
2022-02-17 13:32:58 -0600

    JTN-36 feature: refactored main to log system performance and edit CMake build files

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,14 @@
 #include <iostream>
 #include <iomanip>
 #include <cstdlib>
+#include <thread>
 
 #include "Config.h"
+#include "Performance.h"
 
+void custom_access_log(const std::string& url){
+    // I will probably log something here when clients connect
+}
 class hello_world_resource : public httpserver::http_resource {
 public:
     const std::shared_ptr<httpserver::http_response> render(const httpserver::http_request&) {
@@ -14,6 +19,8 @@ public:
 
 int main(int argc, char** argv) {
     Config& config = Config::get_instance();
+    Performance_Monitoring pm;
+    std::thread t(&Performance_Monitoring::run, pm);
 
     httpserver::webserver ws = httpserver::create_webserver(config.GET_PORT())
         .use_ssl()
@@ -24,7 +31,8 @@ int main(int argc, char** argv) {
         .memory_limit(config.GET_MEMORY_LIMIT())
         .use_dual_stack()
         .start_method(httpserver::http::http_utils::INTERNAL_SELECT)
-        .max_threads(config.GET_MAX_THREADS());
+        .max_threads(config.GET_MAX_THREADS())
+        .log_access(custom_access_log);
 
     hello_world_resource hwr;
     ws.register_resource("/helloworld", &hwr);
