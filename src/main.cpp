2e3e63d6d821796ab3bea266d62c9bfc95c7b803
Calvin <calvindo95@gmail.com>
2022-02-21 16:54:09 -0600

    JTN-197 feature: added checks for CA key and cert and added graceful shutdown for logging thread

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,12 @@
 #include <iomanip>
 #include <cstdlib>
 #include <thread>
+#include <filesystem>
+#include <atomic>
 
-#include "Config.h"
-#include "Performance.h"
-#include "HelloWorldResource.h"
+#include "Config.h"                 // Config
+#include "Performance.h"            // Performance_Monitoring
+#include "HelloWorldResource.h"     // hello_world_resource
 
 void custom_access_log(const std::string& url){
     // I will probably log something here when clients connect
@@ -14,22 +16,43 @@ void custom_access_log(const std::string& url){
 
 int main(int argc, char** argv) {
     Config& config = Config::get_instance();
-    Performance_Monitoring pm;
-    std::thread t(&Performance_Monitoring::run, pm);
 
-    httpserver::webserver ws = httpserver::create_webserver(config.GET_PORT())
+    std::atomic<bool> stop_thread_flag = false;
+    Performance_Monitoring pm(stop_thread_flag);
+    std::thread t(&Performance_Monitoring::run, std::ref(pm));
+
+    httpserver::create_webserver cw = httpserver::create_webserver(config.GET_PORT())
+        // set required parameters
         .use_ssl()
-        .https_mem_key(config.GET_HTTPS_MEM_KEY())
-        .https_mem_cert(config.GET_HTTPS_MEM_CERT())
         .max_connections(config.GET_MAX_CONNECTIONS())
         .connection_timeout(config.GET_CONNECTION_TIMEOUT())
         .memory_limit(config.GET_MEMORY_LIMIT())
-        .use_dual_stack()
         .start_method(httpserver::http::http_utils::INTERNAL_SELECT)
         .max_threads(config.GET_MAX_THREADS())
         .log_access(custom_access_log)
         .digest_auth();
-        
+
+    // check for valid CA key
+    if(std::filesystem::exists(config.GET_HTTPS_MEM_KEY())){
+        cw.https_mem_key(config.GET_HTTPS_MEM_KEY());
+    }
+    else{
+        std::cerr << "ERROR: Could not find CA key" << std::endl;
+        stop_thread_flag = true;    // allow for graceful stop of logging thread
+        exit(1);
+    }
+
+    // check for valid CA cert/crt
+    if(std::filesystem::exists(config.GET_HTTPS_MEM_CERT())){
+        cw.https_mem_cert(config.GET_HTTPS_MEM_CERT());
+    }
+    else{
+        std::cerr << "ERROR: Could not find CA cert" << std::endl;
+        stop_thread_flag = true;    // allow for graceful stop of logging thread   
+        exit(1);
+    }
+
+    httpserver::webserver ws = cw;
     hello_world_resource hwr;
     ws.register_resource("/helloworld", &hwr);
     ws.start(true);
