10ef12ecd4f8c010b8a3aa7deb4fb08aba9d3f1c
Calvin <calvindo95@gmail.com>
2022-02-22 23:55:26 -0600

    JTN-36 feature: added configuration options and total runtime output

--- test/hammer_server.py
+++ test/hammer_server.py
@@ -2,16 +2,19 @@ import grequests
 import threading
 import requests
 from requests.auth import HTTPDigestAuth
+from datetime import datetime
 
 # pip3 install grequests
 
 # This program will get some 401's because requests is not thread safe with digest authentication enabled
 
 NUM_OF_REQUESTS = 250
-NUM_OF_THREADS = 8
+NUM_OF_THREADS = 16     # if this goes above MAX_CONNECTIONS on server, it will stall; NUM_THREADS < MAX_CONNECTIONS
 URL = "https://172.18.0.2:8081/helloworld"
 CERT_PATH = "/usr/local/share/ca-certificates/smoothstack_client.crt"
-BULK_SEND = False       # if set true, will send many requests at once using grequests
+BULK_SEND = True       # if set true, will send many requests at once using grequests
+USER = "calvin"
+PASSWORD = "mypass"
 
 lock = threading.Lock()
 
@@ -19,19 +22,19 @@ def hammer():
     if BULK_SEND:
         # create session
         s = requests.Session()
-        s.auth = HTTPDigestAuth("calvin", "mypass")
+        s.auth = HTTPDigestAuth(USER, PASSWORD)
         s.verify = CERT_PATH
 
         # creates a set of unsent requests with a single session
         rs = (grequests.get(URL, session=s) for _ in range(NUM_OF_REQUESTS))
 
         # send all the requests at once
-        for response in grequests.imap(rs, 10):
+        for response in grequests.imap(rs, 50):
             print(f"{threading.currentThread().getName()} {response}")
 
     else:
         s = requests.Session()
-        s.auth = HTTPDigestAuth("calvin", "mypass")
+        s.auth = HTTPDigestAuth(USER, PASSWORD)
         s.verify = CERT_PATH
 
         for i in range(NUM_OF_REQUESTS):
@@ -40,6 +43,8 @@ def hammer():
 
 if __name__ == "__main__":
     threads = []
+    start = datetime.now()
+
     for i in range(NUM_OF_THREADS):
         thread = threading.Thread(target=hammer)
         thread.start()
@@ -47,3 +52,6 @@ if __name__ == "__main__":
 
     for thread in threads:
         thread.join()
+
+    end = datetime.now()
+    print(f"{NUM_OF_REQUESTS*NUM_OF_THREADS} requests in {end-start} seconds")
