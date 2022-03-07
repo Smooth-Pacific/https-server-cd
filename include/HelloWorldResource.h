912693ddc6336aa21d1c656968f5311731e2f604
Calvin <calvindo95@gmail.com>
2022-03-07 09:43:26 -0600

    JTN-54 feature: added xml endpoint

--- include/HelloWorldResource.h
+++ include/HelloWorldResource.h
@@ -6,3 +6,8 @@ class hello_world_resource : public httpserver::http_resource {
     public:
         const std::shared_ptr<httpserver::http_response> render(const httpserver::http_request& req);
 };
+
+class file_resource : public httpserver::http_resource {
+    public:
+        const std::shared_ptr<httpserver::http_response> render(const httpserver::http_request& req);
+};
